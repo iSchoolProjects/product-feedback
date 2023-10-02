156f13326f437fca368e0d03ace09c12ee52a7d8
milicapq <117392234+milicapq@users.noreply.github.com>
2023-10-02 18:15:01 +0200

    sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Sidebar() {
+  return (
+    <>
+      <div className="sidebar-header">
+        <h4>Frontend Mentor</h4>
+        <p>Feedback Board</p>
+      </div>
+    </>
+  );
+}
