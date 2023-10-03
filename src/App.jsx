df335f2dcc2952998a37bec44c14c45568f5caf3
MZoranovic <117400219+MZoranovic@users.noreply.github.com>
2023-10-03 20:57:33 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import React from "react";
-import "./App.css";
-import Sidebar from "./components/Sidebar";
+import React from 'react';
+import './App.css';
+import Sidebar from './components/Sidebar';
+import { data } from './components/data';
 
 function App() {
-  return <Sidebar />;
+  return <Sidebar products={data.productRequests} />;
 }
 
 export default App;
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,23 @@
-import React from "react";
-
-export default function Sidebar(label) {
-  const pills = ["All", "UI", "UX", "Enchancement", "Bug", "Feature"];
+import React, { useState, useEffect } from 'react';
+// pravimo state za iteme, kad se komponenta ucita pokrenucemo fiju getByStatus i njen rezultat upisujemo u state,  a sam state cemo ispisivati na mjestu gdje treba ispisati iteme
+// kad setujemo state, posto znamo da je objekat, putem objectEntries cemo ispisati podatke, on vraca niz nizova, na taj metod vezujemo map da bi mogli ispisati
+export default function Sidebar({ products, label, status }) {
+  const [item, setItem] = useState();
+  const getByStatus = () => {
+    const newObject = {};
+    for (const current of products) {
+      if (newObject[current.status]) {
+        newObject[current.status]++;
+      } else {
+        newObject[current.status] = 1;
+      }
+    }
+    setItem(newObject);
+  };
+  useEffect(() => {
+    getByStatus();
+  }, []);
+  const pills = ['All', 'UI', 'UX', 'Enchancement', 'Bug', 'Feature'];
   return (
     <aside>
       <div className="sidebar-header">
@@ -20,20 +36,15 @@ export default function Sidebar(label) {
         <div className="title">
           <h4>Roadmap</h4>
           <p>View</p>
+          {/* link */}
         </div>
         <ul>
-          <div>
-            <li>Planed</li>
-            <span>0</span>
-          </div>
-          <div>
-            <li>In-progres</li>
-            <span>0</span>
-          </div>
-          <div>
-            <li>Live</li>
-            <span>0</span>
-          </div>
+          {Object.entries(item ?? {}).map(([name, count]) => (
+            <li className={name}>
+              <span>{name}</span>
+              <span>{count}</span>
+            </li>
+          ))}
         </ul>
       </div>
     </aside>
@@ -47,3 +58,11 @@ export default function Sidebar(label) {
 //u nizu izbacujemo ga, a ako nije ubacujemo
 //u momentu kad duzina niza bude ista kao duzina labela,ispraznicemo niz
 //gdje radimo klase tu pitamo da li se eleemnt nalazi u stejtu i n osnovu toga dajemo klasu active
+
+// kad primimo niz objekata svaki ce imati neki status, posto nisu sortirani trebamo ih organizovati u jedan objekat gdje je kljuc sam ststus a vr koliko se puta status pojavio
+// {planned: 2}
+
+// pravimo objekat prazan i for off lup koji prolazi kroz niz u kojem se nalaze objekti sa statusom, na svaki prolaz postavljamo pitanje o objektu da li se dati status nalazi u tom objektu, ako se nalazi povecavamo vr za 1 a ako ne pravimo kljuc u objeektu sa vr koju nam daje status i dajemo inicijalnu vr 1
+
+// pravimo state za iteme, kad se komponenta ucita pokrenucemo fiju getByStatus i njen rezultat upisujemo u state,  a sam state cemo ispisivati na mjestu gdje treba ispisati iteme
+// kad setujemo state, posto znamo da je objekat, putem objectEntries cemo ispisati podatke, on vraca niz nizova, na taj metod vezujemo map da bi mogli ispisati
