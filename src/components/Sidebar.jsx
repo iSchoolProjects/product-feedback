e3fc2179e7f37026888aac51325c00166f3065f6
milicapq <117392234+milicapq@users.noreply.github.com>
2023-10-06 18:03:40 +0200

    update sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import Buttons from "./Buttons";
 
 export default function Sidebar({ products, label, status }) {
   const [filters, setFilter] = useState([]);
@@ -12,7 +13,7 @@ export default function Sidebar({ products, label, status }) {
         newObject[current.status] = 1;
       }
     }
-
+    console.log(item);
     setItem(newObject);
   };
   useEffect(() => {
@@ -35,7 +36,7 @@ export default function Sidebar({ products, label, status }) {
   // ako ima izbacuje ga, u suprotnom ga dodaje
   // nakon tih provjera, setuje filter state na nove vrijednosti
   //ptreban nam je state, prazan niz je pocetna vr
-  console.log(filters);
+
   const pills = ["All", "UI", "UX", "Enchancement", "Bug", "Feature"];
   return (
     <>
@@ -44,23 +45,12 @@ export default function Sidebar({ products, label, status }) {
           <h3>Frontend Mentor</h3>
           <p>Feedback Board</p>
         </div>
-        <div className="buttons">
-          {pills.map((pill) => (
-            <>
-              <label
-                active={(
-                  filters.includes(pill) ||
-                  (pill === "All" && filters.length === 0)
-                ).toString()}
-                htmlFor={pill}
-                onClick={() => handleClick(pill)}
-              >
-                {pill}{" "}
-              </label>
-              <input type="checkbox" label={label} name="maja" id={pill} />
-            </>
-          ))}
-        </div>
+        <Buttons
+          label={label}
+          filters={filters}
+          handleClick={handleClick}
+          pills={pills}
+        />
         <div className="road-map">
           <div className="title">
             <h4>Roadmap</h4>
@@ -109,19 +99,12 @@ export default function Sidebar({ products, label, status }) {
               <div className="offcanvas-body">
                 {" "}
                 <aside>
-                  <div className="buttons">
-                    {pills.map((pill) => (
-                      <>
-                        <label htmlFor={pill}>{pill} </label>
-                        <input
-                          type="checkbox"
-                          label={label}
-                          name="maja"
-                          id={pill}
-                        />
-                      </>
-                    ))}
-                  </div>
+                  <Buttons
+                    label={label}
+                    filters={filters}
+                    handleClick={handleClick}
+                    pills={pills}
+                  />
                   <div className="road-map">
                     <div className="title">
                       <h4>Roadmap</h4>
@@ -146,29 +129,3 @@ export default function Sidebar({ products, label, status }) {
     </>
   );
 }
-//praviom niz stringova BadghLabels
-//kad provrtimo niz map ce praviti button i imace klasu da zna da li je
-//aktivan ili ne
-//da bi raidlo treba nam stejt, koji je prazan niz za pocetak,
-//  prazan niz je all
-//klikom na odr element, mi toglujemo element u nizu,
-// tj ako je el
-//u nizu izbacujemo ga, a ako nije ubacujemo
-//u momentu kad duzina niza bude ista kao duzina labe
-// kroz niz u kojem se nalaze objekti sa statusom,
-// na svaki prolaz postavljamo pitanje o objektu da li se
-// dati status nalazi u tom objektu, ako se nalazi povecavamo
-// vr za 1 a ako ne pravimo kljuc u objeektu sa vr koju nam
-// daje status i dajemo inicijalnu vr 1
-
-// pravimo state za iteme, kad se komponenta
-// ucita pokrenucemo fiju getByStatus i njen rezultat upisujemo u state,
-//  a sam state cemo ispisivati na mjestu gdje treba ispisati iteme
-// kad setujemo state, posto znamo da je objekat, putem objectEntries
-// cemo ispisati podatke, on vraca niz nizova, na taj metod
-// vezujemo map da bi mogli ispisati
-// pravimo state za iteme, kad se komponenta ucita pokrenucemo fiju
-//getByStatus i njen rezultat upisujemo u state,
-// a sam state cemo ispisivati na mjestu gdje treba ispisati iteme
-// kad setujemo state, posto znamo da je objekat,
-//putem objectEntries cemo ispisati podatke, on vraca niz nizova, na taj metod vezujemo map da bi mogli ispisati
