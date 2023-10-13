5ca34aa1f454dbbb85334011f68cc00abace69b4
Darko22 <retcr2210@gmail.com>
2023-10-13 20:03:51 +0200

    Roadmap another update

--- src/components/Roadmap.jsx
+++ src/components/Roadmap.jsx
@@ -3,7 +3,7 @@ import { data } from '../data';
 import Roadmapcard from './Roadmapcard';
 import { RoadmapMapEnum } from '../RoadmapMap';
 export default function Roadmap({ item = data.productRequests }) {
-  const [activeTab, setActiveTab] = useState('Planned');
+  const [activeTab, setActiveTab] = useState('planned');
   const [cards, setCard] = useState({});
   const getCard = () => {
     const newArray = {};
@@ -22,32 +22,41 @@ export default function Roadmap({ item = data.productRequests }) {
     setActiveTab(tab);
   };
   useEffect(() => {
+    if (window.innerWidth > 390) {
+      setActiveTab();
+    }
     getCard();
   }, []);
+
   return (
     <>
       <div className="roadmap-holder">
         <div className="navigation">
           {Object.entries(cards).map(([key, card]) => (
-            <button
-              className={`${key === activeTab ? 'active-tab' : ''} ${key}`}
-              onClick={() => handleClick(key)}
-            >
-              {key.replace('-', ' ')} ({card.length})
-            </button>
+            <div className="container">
+              <button
+                className={`${key === activeTab ? 'active-tab' : ''} ${key}`}
+                onClick={() => handleClick(key)}
+              >
+                {key.replace('-', ' ')} ({card.length})
+              </button>
+            </div>
           ))}
         </div>
-        {Object.entries(cards).map(([key, card]) => (
+
+        {Object.entries(
+          activeTab ? { [activeTab]: cards[activeTab] } : cards
+        ).map(([key, card]) => (
           <div className="main-container">
             <div className="schedule">
               <h2>
-                {key.replace('-', ' ')} ({card.length})
+                {key.replace('-', ' ')} ({card?.length})
               </h2>
 
               <p> {RoadmapMapEnum[key]}</p>
             </div>
 
-            {card.map((c) => (
+            {card?.map((c) => (
               <Roadmapcard item={c} />
             ))}
           </div>
@@ -61,6 +70,10 @@ export default function Roadmap({ item = data.productRequests }) {
 // <div>{card?.filter((item)=> activeTab===item['status']).map(({id,...rest})=><Roadmapcard {...{...
 // }
 
-//
+//  {key === activeTab ? card.map((c) => <Roadmapcard item={c} />) : ''} ?
 
 // ?
+// {
+//   a,b,c,d
+// }
+// {a}
