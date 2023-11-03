42558a4a0e8d77e73a76c16bc1641b9f5e54e122
Jelena406 <117379663+Jelena406@users.noreply.github.com>
2023-11-03 20:30:24 +0100

    update design

--- src/components/Roadmapcard.jsx
+++ src/components/Roadmapcard.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function Roadmapcard({ item }) {
   const navigate = useNavigate();
--- src/components/SuggestionsList.jsx
+++ src/components/SuggestionsList.jsx
@@ -1,7 +1,7 @@
-import { Consumer } from '../App';
-import Suggestion from './Suggestion';
-import React, { useContext } from 'react';
-import NoFeedback from './NoFeedback';
+import { Consumer } from "../App";
+import Suggestion from "./Suggestion";
+import React, { useContext } from "react";
+import NoFeedback from "./NoFeedback";
 
 export default function SuggestionsList() {
   const { feedbacks } = useContext(Consumer);
